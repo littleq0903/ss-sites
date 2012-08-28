f50221426bf4c7532cd9dc902394bb8dd8bdc507
LittleQ <littleq0903@gmail.com>
2012-08-28 22:13:36 +0800

    modified settings

--- studynet/configs/heroku/settings.py
+++ studynet/configs/heroku/settings.py
@@ -1,3 +1,8 @@
 from ..common.settings import *
 
+import dj_database_url
+
+DATABASES = {
+        'default': dj_database_url.config(default='postgres://jhbsaudowefhif:s3SziM9NR1s2FdkX-g6Yf7G27-@ec2-107-22-164-173.compute-1.amazonaws.com:5432/d421lpe5fkgv6e')
+}
 
