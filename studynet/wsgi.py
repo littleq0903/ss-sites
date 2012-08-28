eeccd1775e1ef6c2e52d311173cf85902a985a47
LittleQ <littleq0903@gmail.com>
2012-08-28 21:47:07 +0800

    fixed the wrong position of config file for heroku

--- studynet/wsgi.py
+++ studynet/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configs.heroku.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studynet.configs.heroku.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
