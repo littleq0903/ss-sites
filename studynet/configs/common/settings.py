57bcf94b6dae5601d50268947f7596ce00eba4ca
snowmantw <snowmantw@gmail.com>
2012-08-29 18:58:13 +0800

    [modify] info -- Database setting in settings.py

--- studynet/configs/common/settings.py
+++ studynet/configs/common/settings.py
@@ -12,7 +12,8 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-        'default': dj_database_url.config(default='postgres://localhost/nccustudy')
+#        'default': dj_database_url.config(default='postgres://localhost/nccustudy')
+         'default': dj_database_url.config(default='postgres://jhbsaudowefhif:s3SziM9NR1s2FdkX-g6Yf7G27-@ec2-107-22-164-173.compute-1.amazonaws.com:5432/d421lpe5fkgv6e')
 }
 
 # Local time zone for this installation. Choices can be found here:
