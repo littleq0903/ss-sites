aa397398036089d7e393189101a428177ed6e6a4
LittleQ <littleq0903@gmail.com>
2012-08-29 22:04:17 +0800

    switched to a better function for serving static files

--- studynet/urls.py
+++ studynet/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,6 +18,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += patterns('',
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-)
+
+urlpatterns += staticfiles_urlpatterns()
