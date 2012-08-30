97073b9349c998de53dae299eacf92175cdf15ab
LittleQ <littleq0903@gmail.com>
2012-08-31 00:08:42 +0800

    added landing page

--- studynet/apps/courses/models.py
+++ studynet/apps/courses/models.py
@@ -4,23 +4,21 @@ from django.db import models
 
 class CourseData(models.Model):
     """
-        Model for courses
+        Model for courses data from school
         fs_: prefix stands for this attribute came from school data.
     """
 # From school data
     fs_semester = models.CharField(max_length=5)
     fs_course_number = models.CharField(max_length=20)
-    fs_teacher_chinese = models.CharField(max_length=40)
-    fs_teacher_english = models.CharField(max_length=40)
+    fs_teacher = models.CharField(max_length=40)
     fs_credit_point = models.CharField(max_length=20, blank=True)
     fs_class_time = models.CharField(max_length=40, blank=True)
     fs_class_room = models.CharField(max_length=40, blank=True)
     fs_is_common_course = models.BooleanField(default=False)
     fs_common_course_category = models.CharField(max_length=20, blank=True)
+    fs_choose_method = models.CharField(max_length=20, blank=True)
     fs_syllabus_link = models.URLField(blank=True)
     fs_creater = models.CharField(max_length=50)
     fs_full_times = models.IntegerField()
 
-    # attribute
-    uuid = models.CharField(max_length=40)
 
