0f3f22077668ac2839460b0ee0bd0617f6d1b8c6
snowmantw <snowmantw@gmail.com>
2012-09-01 00:01:41 +0800

    [modify] extension -- Memoized UI functions ( BUGGY ); labels to show courses added

--- extension/social-nccu/chrome/social_plugins_course.js
+++ extension/social-nccu/chrome/social_plugins_course.js
@@ -1,6 +1,20 @@
 
 var tmpl = {}
-tmpl['btn_social_course'] = "<span class='btn social-course'>加入到 Social Study</span>"
+tmpl['btn_social_course'] = function(courseId)
+{   
+    return "<span class='btn social-course' id='c"+courseId+"'>加入到 Social Study</span>" 
+}
+
+tmpl['label_forward_done'] = function(courseId)
+{
+    var t = 
+    [ "<span class='label forward-done' id='c"+courseId+"'>已加入"
+    , "<a href='"+URL_ROOT+"/courses/"+SCHOOL+"/"+courseId+"' target='_blank'>StudyNet</a>"
+    , "</span>"
+    ].join("")
+
+    return t
+}
 
 var URL_ROOT = "http://127.0.0.1:8090"
 var SCHOOL = "nccu"
@@ -14,44 +28,57 @@ var isQryPage = function()
 }
 
 // UI () -> UI [DOM]
-var fn$QryDescRow = function()
-{
-    return $('.maintain_profile_content_table tr[id]')
-        .filter
-         (  function()
-            {   return null!= ($(this).attr('id')).match("_Qrytt$")  }
-         )
-}
+var fn$QryDescRow = _.memoize
+(   function()
+    {
+        return $('.maintain_profile_content_table tr[id]')
+            .filter
+             (  function()
+                {   return null!= ($(this).attr('id')).match("_Qrytt$")  }
+             )
+    }
+)
 
-var fn$QryDataRow = function()
-{
-    var data_rows = $('.maintain_profile_content_table tr[id]')
-        .filter(function(){ {   return null!= ($(this).attr('id')).match("_QryTr$")  }})
-        .clone()    // prevent ui change after add some cells.
-        .toArray()
+// NOTE: Get single row will be a DOM Row, not a (UI Row)
+// UI [DOM]
+var fn$QryDataRow = _.memoize
+(    function()
+     {
+        var data_rows = $('.maintain_profile_content_table tr[id]')
+            .filter(function(){ {   return null!= ($(this).attr('id')).match("_QryTr$")  }})
+            .clone()    // prevent ui change after add some cells.
+            .toArray()
 
-    // Unfortunately, the name of course is not in the info row.
-    // We should manually add it into the DOM.
-    var desc_rows = fn$QryDescRow().toArray()
-    _.each
-    (   desc_rows
-    ,   function(row, idx)
-        {   // The CourseName cell is the head of a description row.
-            // Clone it's infomation and append it into data row.
-            $(data_rows[idx]).children('td[class]:last')
-                .after( $(row).children('td[class]:first').clone().hide() )
-        }
-    )
+        // Unfortunately, the name of course is not in the info row.
+        // We should manually add it into the DOM.
+        var desc_rows = fn$QryDescRow().toArray()
+        _.each
+        (   desc_rows
+        ,   function(row, idx)
+            {   // The CourseName cell is the head of a description row.
+                // Clone it's infomation and append it into data row.
+                $(data_rows[idx]).children('td[class]:last')
+                    .after( $(row).children('td[class]:first').clone().hide() )
+            }
+        )
 
-    return $(data_rows)
-}
+        return $(data_rows)
+    }
+)
 
 // ---- 
 
-// UI [DOM] -> UI [Buttom]
-var addBtn = function($doms)
+// UI [DOM], [CourseId] -> UI [Buttom]
+var addBtn = function($doms, ids)
 {
-    $doms.find('span[id]:first').after($(tmpl['btn_social_course']))
+    $doms.find('span[id]:first')
+         .each
+          ( function(idx)
+            {
+                var btn = tmpl['btn_social_course'](ids[idx])
+                $(this).after(btn)
+            }
+          )
 
     // Forward the event.
     $doms.find('.btn.social-course')
@@ -66,46 +93,48 @@ var addBtn = function($doms)
 // Parse the Qry row and get infos
 //
 // UI DOM -> QryResult
-var parseInfoRow = function($row)
-{
-    var __text = function(td){ return $(td).text().replace(/\s/g,"") }
-    var parseSlots = 
-    [   { 'toTraceList': function(td){ return $(td).find('input:first').attr('name')} }
-    ,   { 'semester'   : __text }
-    ,   { 'courseId'   : __text }
-    ,   { 'teacher'    : __text }
-    ,   { 'credit'     : __text }
-    ,   { 'dateTime'   : __text }
-    ,   { 'place'      : __text }
-    ,   { 'syllabus'   : function(td){ return $(td).find('input:first').attr('name')}  }
-    ,   { 'way'        : __text }
-    ,   { 'isRemote'   : __text }
-    ,   { 'language'   : __text }
-    ,   { 'classGECL'  : __text }
-    ,   { 'charge'     : __text }
-    ,   { 'auxiliary'  : __text }
-    ,   { 'department' : __text }
-    ,   { 'volume'     : __text } // course occurs 1 or 2 semester(s)
-    ,   { 'category'   : __text } // required | elective... 
-    ,   { 'isKernel'   : __text }
-    ,   { 'numLeft'    : __text }
-    ,   { 'numQueue'   : __text }
-    ,   { 'courseName' : __text }
-    ]
+var parseInfoRow = _.memoize
+(   function($row)
+    {
+        var __text = function(td){ return $(td).text().replace(/\s/g,"") }
+        var parseSlots = 
+        [   { 'toTraceList': function(td){ return $(td).find('input:first').attr('name')} }
+        ,   { 'semester'   : __text }
+        ,   { 'courseId'   : __text }
+        ,   { 'teacher'    : __text }
+        ,   { 'credit'     : __text }
+        ,   { 'dateTime'   : __text }
+        ,   { 'place'      : __text }
+        ,   { 'syllabus'   : function(td){ return $(td).find('input:first').attr('name')}  }
+        ,   { 'way'        : __text }
+        ,   { 'isRemote'   : __text }
+        ,   { 'language'   : __text }
+        ,   { 'classGECL'  : __text }
+        ,   { 'charge'     : __text }
+        ,   { 'auxiliary'  : __text }
+        ,   { 'department' : __text }
+        ,   { 'volume'     : __text } // course occurs 1 or 2 semester(s)
+        ,   { 'category'   : __text } // required | elective... 
+        ,   { 'isKernel'   : __text }
+        ,   { 'numLeft'    : __text }
+        ,   { 'numQueue'   : __text }
+        ,   { 'courseName' : __text }
+        ]
 
-    var tds = $row.find('td')
-    return _.reduce
-    (   tds
-    ,   function(result, td, idx)
-        {
-            var key = _.keys(parseSlots[idx])[0]
-            var fn  = _.values(parseSlots[idx])[0]
-            result[key] = fn(td)
-            return result
-        }
-    ,   {}
-    )
-}
+        var tds = $row.find('td')
+        return _.reduce
+        (   tds
+        ,   function(result, td, idx)
+            {
+                var key = _.keys(parseSlots[idx])[0]
+                var fn  = _.values(parseSlots[idx])[0]
+                result[key] = fn(td)
+                return result
+            }
+        ,   {}
+        )
+    }
+)
 
 // ----
 
@@ -141,6 +170,17 @@ note.cb['social-course'] = function(name, data)
 
     // Flush background parsed data with this one.
     ioForwardCourse( parseInfoRow($row)['courseId'] ).flush()
+}
+
+note.cb['social-course.forward_done'] = function(name, ids)
+{
+    _.each
+    (   ids
+    ,   function(id)
+        {   // Find the button with courseId, and replace it.
+            $('#'+'c'+id).replaceWith(tmpl['label_forward_done'](id))
+        }
+    )
 }
 
 // Each signal bring left rows of data.
@@ -210,12 +250,14 @@ var ioForwardCourse= function(id)
          .success
           ( function()
             {
+                note('social-course.forward_done', ids)
             }
           )
          .error
           ( function()
             {
-
+                //FIXME: only for test
+                note('social-course.forward_done', ids)
             }
           )
     }
@@ -230,7 +272,17 @@ var main = function()
 {
     if( isQryPage )
     {
-        addBtn(fn$QryDescRow())
+        var courseIds = function()
+        {
+            return _.map
+            (   fn$QryDataRow()
+            ,   function(dom_row)
+                {   return parseInfoRow($(dom_row))['courseId']
+                }
+            )
+        }
+
+        addBtn(fn$QryDescRow(), courseIds() )
         note('signal.collect_result',fn$QryDataRow().toArray())
     }
 }
