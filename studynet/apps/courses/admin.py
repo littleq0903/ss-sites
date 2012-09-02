d469c3ebbb5b12c92a3c7416fe10096dc17996af
LittleQ <littleq0903@gmail.com>
2012-09-03 01:11:29 +0800

    add admin for clicking

--- studynet/apps/courses/admin.py
+++ studynet/apps/courses/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from courses.models import CourseData
+from courses.models import CourseData, CourseClick
 
 
 class CourseDataAdmin(admin.ModelAdmin):
     list_display = ('fs_semester', 'fs_course_number', 'fs_teacher', 'department', 'fs_is_common_course')
 
+
+class CourseClickAdmin(admin.ModelAdmin):
+    list_display = ('course', 'click')
+
+admin.site.register(CourseClick, CourseClickAdmin)
 admin.site.register(CourseData, CourseDataAdmin)
