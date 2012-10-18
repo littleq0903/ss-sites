6ee7f66f2e8cfbc81709f6502748c3b0efa1b124
LittleQ <littleq0903@gmail.com>
2012-10-18 20:42:01 +0800

    added tornado frontend code

---
+++ tornado_push/pushd.py
@@ -0,0 +1,33 @@
+import tornado.ioloop
+import tornado.web
+from tornado import websocket
+
+GLOBALS={
+    'sockets': []
+}
+
+
+class ClientSocket(websocket.WebSocketHandler):
+    def open(self):
+        GLOBALS['sockets'].append(self)
+        print "WebSocket opened"
+
+    def on_close(self):
+        print "WebSocket closed"
+        GLOBALS['sockets'].remove(self)
+
+class Announcer(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        data = self.get_argument('data')
+        for socket in GLOBALS['sockets']:
+            socket.write_message(data)
+        self.write('Posted')
+
+application = tornado.web.Application([
+    (r"/socket", ClientSocket),
+    (r"/push", Announcer),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
