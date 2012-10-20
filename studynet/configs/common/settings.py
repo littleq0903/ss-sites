bd3f51c5cfa42bf748615a4c8e03189483048309
LittleQ <littleq0903@gmail.com>
2012-10-20 10:32:07 +0800

    removed useless settings in settings.py(commmon)

--- studynet/configs/common/settings.py
+++ studynet/configs/common/settings.py
@@ -1,6 +1,5 @@
 # Django settings for studynet project.
 
-import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -13,10 +12,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-        #'default': dj_database_url.config(default='postgres://jhbsaudowefhif:s3SziM9NR1s2FdkX-g6Yf7G27-@ec2-107-22-164-173.compute-1.amazonaws.com:5432/d421lpe5fkgv6e')
-        'default': dj_database_url.config(default='postgres://localhost:5432/studynet_test')
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
